6f1c0a354f709b9a90d8ab1528197f264a0a955e
iban-nabi <85503806+iban-nabi@users.noreply.github.com>
2025-06-17 16:12:20 +0800

    Create UserController.java

---
+++ src/main/java/com/ecommerce/store/controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.ecommerce.store.controllers;
+
+import com.ecommerce.store.entities.User;
+import com.ecommerce.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id){
+        User user = userRepository.findById(id).orElse(null);
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(user);
+    }
+
+}
